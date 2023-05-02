d451940acaf3142e814ceff37ebafbd811b27ec0
Hector Vidal Candelario Cedeño <94994376+hectorvcc@users.noreply.github.com>
2023-05-02 14:21:49 +0200

    Principio del proyecto
    Hemos creado un proyecto nuevo para el Hackaton

---
+++ spring-boot-aws/spring-boot-aws/src/main/java/com/amazon/springbootaws/SpringBootAwsApplication.java
@@ -0,0 +1,27 @@
+package com.amazon.springbootaws;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringBootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsApplication.class, args);
+
+
+		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+		// Listing all the buckets
+		List<Bucket> buckets = s3.listBuckets();
+		// Iterating through the bucket
+		buckets.stream().forEach(bucket -> {
+			System.out.println("Bucket Name:" + bucket.getName() + ", Bucket Owner" + bucket.getOwner().getDisplayName()
+					+ ", Bucket Creation Date :" + bucket.getCreationDate());
+		});
+	}
+}
