b95548dcd98488d7ee2ccba664b1019e4bc16031
Hector Vidal Candelario Cedeño <94994376+hectorvcc@users.noreply.github.com>
2023-05-02 14:26:12 +0200

    Principio del proyecto

---
+++ spring-boot-aws/src/test/java/com/amazon/springbootaws/SpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
